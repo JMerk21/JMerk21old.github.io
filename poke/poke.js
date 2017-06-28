fc0108a0637f24cdd8c44ba4d7fb5fc0b53958b5
Jaeden <jmerkley21@gmail.com>
2017-06-28 11:13:59 -0600

    CIT230 API exercise

--- poke/poke.js
+++ poke/poke.js
@@ -10,11 +10,27 @@ function getJSON(url) {
 }
 
 //lets get some ships
-function fetchPokes() {
-    var url = "//swapi.co/api/starships/";
+function fetchMons() {
+    var url = "//pokeapi.co/api/v2/type/16/";
     //call getJSON function to get the list of ships from the api
     getJSON(url).then(function (data) {
         //stuff that should happen after the request is done.
         console.log(data);
+        var results = data.results;
+        var monListElement = document.getElementById('monList');
+        monListElement.innerHTML = "";
+
+        results.forEach(function (mons) {
+            var listItem = document.createElement('li');
+            var link = document.createElement('a');
+
+            link.setAttribute('href', mons.url);
+
+            link.innerHTML = mons.name;
+
+            listItem.appendChild(link);
+
+            monListElement.appendChild(listItem);
+        });
     });
 }
