0c4b7ac4ab0a3fddcd7c91ed9c37095aba23000c
Jaeden <jmerkley21@gmail.com>
2017-07-07 08:53:19 -0600

    Added function

--- jsonweather-html/scripts/remote_wu.js
+++ jsonweather-html/scripts/remote_wu.js
@@ -52,3 +52,15 @@
         });
     }
 }());
+
+jQuery(document).ready(function($) {
+    $.ajax({
+        url : "http://api.wunderground.com/api/ba98fbce1faceef0/geolookup/conditions/q/IA/Cedar_Rapids.json",
+        dataType : "jsonp",
+        success : function(parsed_json) {
+            var location = parsed_json['location']['city'];
+            var temp_f = parsed_json['current_observation']['temp_f'];
+            alert("Current temperature in " + location + " is: " + temp_f);
+        }
+    });
+});
